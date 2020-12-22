8f6c5d23c23546b100a7e68014209f8502ebbec2
Michael Watts <wattsjmichael@gmail.com>
2020-12-22 14:29:58 -0800

    Runs in local host, but no sound

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-
-import { withAuthenticator } from 'aws-amplify-react';
-
-import { XR } from 'aws-amplify';
-import sceneConfig from './sumerian_exports';
-
-XR.configure({ // XR category configuration
-  SumerianProvider: { // Sumerian specific configuration
-    region: 'eu-central-1', // Sumerian region
-    scenes: {
-      "scene1": { // Friendly scene name
-        sceneConfig // Scene configuration from Sumerian publish
-      }
-    },
-  }
-});
+import { SumerianScene } from 'aws-amplify-react';
+import Amplify from 'aws-amplify';
+import Aws_exports from './aws-exports';
+import '@aws-amplify/ui/dist/style.css';
+   
+Amplify.configure(Aws_exports);
 
 class App extends Component {
-  async componentDidMount() {
-    await XR.loadScene("scene1", "sumerian-scene-dom-id");
-    XR.start("scene1");
-  }
   render() {
     return (
-      <div className="App">
-        <div id="sumerian-scene-dom-id"></div>
+      <div style={ { height: '100vh' } }>
+        <SumerianScene sceneName='travbot'/>
       </div>
     );
   }
-}
+};
 
-export default withAuthenticator(App, { includeGreetings: true });
+export default App;
